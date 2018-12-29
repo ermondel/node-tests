9d3da609b5755d75efebdcf559b80967a250c38a
ermondel <ermondel@gmail.com>
2018-12-29 21:57:33 +0200

    test: add testing of express app

--- server/server.js
+++ server/server.js
@@ -3,7 +3,20 @@ const express = require('express');
 let app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello world!');
+    res.status(404).send({
+        error: 'Page not found.',
+        name: 'Todo App v1.0'
+    });
+});
+
+app.get('/users', (req, res) => {
+    res.send([
+        { name: 'Max', age: 21 },
+        { name: 'Peter', age: 23 },
+        { name: 'Djon', age: 27 }
+    ]);
 });
 
 app.listen(3000);
+
+module.exports.app = app;
